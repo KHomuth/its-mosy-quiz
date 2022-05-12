35de4b5d9a37d3cc95d8e7c05ece56f9b42590ed
Kevin Homuth <kevin.homuth@haw-hamburg.de>
2022-05-12 08:10:54 +0200

    navigate between landingpage and subpages

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LandingPage from './components/LandingPage';
+import QuizPage from './components/QuizPage';
+import InfoPage from './components/InfoPage';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +13,14 @@ export default function App() {
         <Stack.Screen
           name='Home'
           component={LandingPage}
+        />
+        <Stack.Screen
+          name='Quiz'
+          component={QuizPage}
+        />
+        <Stack.Screen
+          name='Infos'
+          component={InfoPage}
         />  
       </Stack.Navigator>
     </NavigationContainer>
--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,35 +1,37 @@
 import * as React from "react";
-import { StyleSheet, Text, View, Button, Linking } from "react-native";
+import { StyleSheet, Text, View, Button, Linking, Pressable } from "react-native";
 
 export default function LandingPage({ navigation }) {
-  
-
     return (
+      <View style={styles.containerBackground}>
         <View style={styles.container}>
-          <Text style={styles.titleText}>Life under Water</Text>
-          <Button title="Quiz starten!" color="lightblue" onPress={() => {
-              Linking.openURL('https://de.wikipedia.org/wiki/Quiz');
-            }}/>
-          <Button title="Mehr Infos?" color="lightblue" onPress={() => {
-              Linking.openURL('https://de.wikipedia.org/wiki/Ozean');
-            }}/>
+          <Text style={[styles.titleText, styles.fontColorWhite]}>Life under Water</Text>
+          <Pressable onPress={() => navigation.navigate('Quiz')}>
+            <Text style={styles.fontColorWhite}>Quiz starten!</Text>
+          </Pressable>
+          <Pressable onPress={() => navigation.navigate('Infos')}>
+            <Text style={styles.fontColorWhite}>Mehr Infos?</Text>
+          </Pressable>
         </View>
+      </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    containerBackground: {
       flex: 1,
       backgroundColor: '#003ca0',
       alignItems: 'center',
-      
-      
+    },
+    container: {
+      top: 85,
+      position: 'absolute'
     },
     titleText: {
       fontWeight: 'bold',
-      fontSize: 28,
-      top: 85,
-      color: '#ffffff',
-      position: 'absolute'
+      fontSize: 28
+    },
+    fontColorWhite: {
+      color: '#ffffff'
     }
 });
